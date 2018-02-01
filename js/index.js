7022a2111ed29b3ad3f5e1fd495e201dd18d1bad
chendong <15273119291@163.com>
2018-02-01 18:56:21 +0800

    Date 获取某月有多少天  闰年的修改

--- js/index.js
+++ js/index.js
@@ -324,7 +324,7 @@ class DateFn {
 
         //当月份为二月时，根据闰年还是非闰年判断天数
         if (mouth == 2) {
-            days = year % 4 == 0 ? 29 : 28
+            days = (year%4==0 && year%100==0 && year%400==0) || (year%4==0 && year%100!=0) ? 28 : 29
         } else if (mouth == 1 || mouth == 3 || mouth == 5 || mouth == 7 || mouth == 8 || mouth == 10 || mouth == 12) {
             //月份为：1,3,5,7,8,10,12 时，为大月.则天数为31；
             days = 31
