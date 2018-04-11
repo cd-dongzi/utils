c5a3303b4abf497a6d964641956347bff10a1cdc
chendong <15273119291@163.com>
2018-04-11 14:20:35 +0800

    添加深度克隆方法

--- js/other.js
+++ js/other.js
@@ -1,4 +1,72 @@
 class OtherFn {
+
+    /**
+     * [deepClone 深度克隆]
+     * @param  {[type]} obj [克隆对象]
+     * @return {[type]}     [返回深度克隆后的对象]
+     */
+    deepClone (obj) {
+        if (obj === null || typeof obj !== 'object') return obj
+        var isType = function(obj, type) {
+            var flag,
+                typeString = Object.prototype.toString.call(obj)
+            switch(type) {
+                case 'Array':
+                    flag = typeString === '[object Array]'
+                    break
+                case 'Date':
+                    flag = typeString === '[object Date]'
+                    break
+                case 'RegExp':
+                    flag = typeString === '[object RegExp]'
+                    break
+                default:
+                    flag = false
+            }
+            return flag
+        }
+        var getRegExp = function(re) {
+            var flags = ''
+            if (re.global) flags += 'g'
+            if (re.ignoreCase) flags += 'i'
+            if (re.multiline) flags += 'm'
+            return flags
+        }
+
+        var _clone = function(parent) {
+            var child, proto, parents = [], children = []
+            if (isType(parent, 'Array')) {// 对数组做特殊处理
+                child = [];
+            } else if (isType(parent, 'RegExp')) {// 对正则做特殊处理
+                child = new RegExp(parent.source, getRegExp(parent));
+                if (parent.lastIndex) child.lastIndex = parent.lastIndex;
+            } else if (isType(parent, 'Date')) {// 对Date做特殊处理
+                child = new Date(parent.getTime());
+            } else {
+                // 处理对象原型
+                proto = Object.getPrototypeOf(parent);
+                // 利用Object.create切断原型链
+                child = Object.create(proto);
+            }
+            // 处理循环引用
+            var index = parents.indexOf(parent);
+
+            if (index != -1) {
+                // 如果父数组存在本对象,说明之前已经被引用过,直接返回此对象
+                return children[index];
+            }
+            parents.push(parent);
+            children.push(child);
+
+            for (var i in parent) {
+                child[i] = _clone(parent[i]);
+            }
+
+            return child;
+        }
+        return _clone(obj)
+    }
+
     /*获取网址参数*/
     getUrlParams(name){
         var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
